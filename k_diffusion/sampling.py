d9d971dcaea1cc99de098afaec84233d33ad640e
Alex Birch <Birch-san@users.noreply.github.com>
2022-12-03 00:22:55 +0000

    DDIM cutoff idea from @marunine (do first-order step if sigma is beneath this threshold) -- generalization of Cheng Lu's idea to do a first-order step on the final step of a low-step-count sample. suggestion was to set value to 0.1. I think for low-step samples the one sigma beneath 0.1 may be 0 anyway, meaning this would be no different than the original algorithm. didn't try for higher-step samples or higher rho though.

--- k_diffusion/sampling.py
+++ k_diffusion/sampling.py
@@ -582,7 +582,7 @@ def sample_dpmpp_sde(model, x, sigmas, extra_args=None, callback=None, disable=N
 
 
 @torch.no_grad()
-def sample_dpmpp_2m(model, x, sigmas, extra_args=None, callback=None, disable=None, warmup_lms=False):
+def sample_dpmpp_2m(model, x, sigmas, extra_args=None, callback=None, disable=None, warmup_lms=False, ddim_cutoff=0.):
     """DPM-Solver++(2M)."""
     extra_args = {} if extra_args is None else extra_args
     s_in = x.new_ones([x.shape[0]])
@@ -601,7 +601,7 @@ def sample_dpmpp_2m(model, x, sigmas, extra_args=None, callback=None, disable=No
             s = t + r * h
             x_2 = (sigma_fn(s) / sigma_fn(t)) * x - (-h * r).expm1() * denoised
             denoised_i = model(x_2, sigma_fn(s) * s_in, **extra_args)
-        elif sigmas[i + 1] == 0 or old_denoised is None:
+        elif sigmas[i+1] <= ddim_cutoff or old_denoised is None:
             denoised_i = denoised
         else:
             h_last = t - t_fn(sigmas[i - 1])
