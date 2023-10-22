b2b1cabcad1ffface0d254ee0b2341d76ecfdf45
Alex Birch <Birch-san@users.noreply.github.com>
2023-10-22 22:43:17 +0100

    compute_metrics: WDS support

--- compute_metrics.py
+++ compute_metrics.py
@@ -4,11 +4,13 @@ import k_diffusion as K
 import importlib.util
 from pathlib import Path
 import torch
-from torch import distributed as dist, multiprocessing as mp
+from torch import distributed as dist, multiprocessing as mp, Tensor
 from torch.utils import data
 from torchvision import datasets, transforms
 from functools import partial
-from typing import Dict, Optional
+from typing import Dict, Optional, Literal, Tuple
+from PIL import Image
+from io import BytesIO
 
 def ensure_distributed():
     if not dist.is_initialized():
@@ -29,6 +31,30 @@ def get_train_set(dataset_config: Dict, config_path: str, tf: Optional[transform
             train_set = load_dataset(dataset_config['location'])
             train_set.set_transform(partial(K.utils.hf_datasets_augs_helper, transform=tf, image_key=dataset_config['image_key']))
             train_set = train_set['train']
+        case 'wds' | 'wds-class':
+            from webdataset import WebDataset
+            def img_from_sample(sample: Dict) -> Tensor:
+                img_bytes: bytes = sample[dataset_config['image_key']]
+                with BytesIO(img_bytes) as stream:
+                    img: Image.Image = Image.open(stream)
+                    img.load()
+                transformed_tensor: Tensor = tf(img)
+                return transformed_tensor
+            def map_labeled_wds_sample(sample: Dict) -> Tuple[Image.Image, int]:
+                img: Tensor = img_from_sample(sample)
+                label: int = sample[dataset_config['label_key']]
+                return (img, label)
+            def map_wds_sample(sample: Dict) -> Tuple[Image.Image]:
+                img: Tensor = img_from_sample(sample)
+                return (img,)
+            match dataset_config['type']:
+                case 'wds':
+                    mapper = map_wds_sample
+                case 'wds-class':
+                    mapper = map_labeled_wds_sample
+                case _:
+                    raise ValueError('')
+            train_set = WebDataset(dataset_config['location']).map(mapper).shuffle(1000)
         case 'custom':
             location = (Path(config_path).parent / dataset_config['location']).resolve()
             spec = importlib.util.spec_from_file_location('custom_dataset', location)
@@ -55,9 +81,6 @@ def main():
     p.add_argument('--evaluate-with', type=str, default='inception',
                    choices=['inception', 'clip', 'dinov2'],
                    help='the feature extractor to use for evaluation')
-    # p.add_argument('--transform-interp', type=transforms.InterpolationMode,
-    #                default=transforms.InterpolationMode.BICUBIC,
-    #                help='which interpolation mode used for resizing ground-truth images')
     p.add_argument('--clip-model', type=str, default='ViT-B/16',
                    choices=K.evaluation.CLIPFeatureExtractor.available_models(),
                    help='the CLIP model to use to evaluate')
@@ -67,6 +90,13 @@ def main():
     p.add_argument('--mixed-precision', type=str,
                    choices=['no', 'fp16', 'bf16', 'fp8'],
                    help='the mixed precision type')
+    p.add_argument('--num-workers', type=int, default=8,
+                   help='the number of data loader workers')
+    p.add_argument('--seed', type=int,
+                   help='the random seed')
+    p.add_argument('--start-method', type=str, default='spawn',
+                   choices=['fork', 'forkserver', 'spawn'],
+                   help='the multiprocessing start method')
     args = p.parse_args()
 
     mp.set_start_method(args.start_method)
@@ -80,7 +110,11 @@ def main():
     ensure_distributed()
     device = accelerator.device
 
-    config_pred = K.config.load_config(args.config_pred, use_json5=args.config.endswith('.jsonc'))
+    if args.seed is not None:
+        seeds = torch.randint(-2 ** 63, 2 ** 63 - 1, [accelerator.num_processes], generator=torch.Generator().manual_seed(args.seed))
+        torch.manual_seed(seeds[accelerator.process_index])
+
+    config_pred, config_target = (K.config.load_config(config, use_json5=config.endswith('.jsonc')) for config in (args.config_pred, args.config_target))
     model_config = config_pred['model']
     pred_dataset_config = config_pred['dataset']
 
@@ -88,15 +122,10 @@ def main():
     assert len(model_config['input_size']) == 2 and model_config['input_size'][0] == model_config['input_size'][1]
     size = model_config['input_size']
 
-    config_target = K.config.load_config(args.config_target, use_json5=args.config.endswith('.jsonc'))
     target_dataset_config = config_target['dataset']
 
-    pred_image_key, target_image_key = (config.get('image_key', 0) for config in (pred_dataset_config, target_dataset_config))
-    num_classes = pred_dataset_config.get('num_classes', 0)
-    cond_dropout_rate = pred_dataset_config.get('cond_dropout_rate', 0.1)
-    class_key = pred_dataset_config.get('class_key', 1)
-
     tf = transforms.Compose([
+        transforms.ToTensor(),
         transforms.Resize(size[0], interpolation=transforms.InterpolationMode.BICUBIC),
         transforms.CenterCrop(size[0]),
     ])
@@ -107,7 +136,6 @@ def main():
             (target_dataset_config, args.config_target),
         )
     )
-    target_train_set = get_train_set(dataset_config=target_dataset_config, config_path=args.config_target, tf=tf)
 
     if accelerator.is_main_process:
         try:
@@ -116,7 +144,7 @@ def main():
         except TypeError:
             pass
 
-    pred_train_dl, target_train_dl = (data.DataLoader(train_set, args.batch_size, shuffle=True, drop_last=True, num_workers=args.num_workers, persistent_workers=True, pin_memory=True) for train_set in (pred_train_set, target_train_set))
+    pred_train_dl, target_train_dl = (data.DataLoader(train_set, args.batch_size, shuffle=not isinstance(train_set, data.IterableDataset), drop_last=True, num_workers=args.num_workers, persistent_workers=True, pin_memory=True) for train_set in (pred_train_set, target_train_set))
     pred_train_dl, target_train_dl = accelerator.prepare(pred_train_dl, target_train_dl)
 
     match args.evaluate_with:
@@ -131,10 +159,19 @@ def main():
     
     pred_train_iter, target_train_iter = (iter(dl) for dl in (pred_train_dl, target_train_dl))
     if accelerator.is_main_process:
-        for source_name, iter, image_key in zip(('pred', 'target'), (pred_train_iter, target_train_iter), (pred_image_key, target_image_key)):
+        features: Dict[Literal['pred', 'target'], Optional[Tensor]] = { 'pred': None, 'target': None }
+        for source_name, iter_ in zip(('pred', 'target'), (pred_train_iter, target_train_iter)):
             print(f'Computing features for {source_name}...')
-            features = K.evaluation.compute_features(accelerator, lambda _: next(iter)[image_key][1], extractor, args.evaluate_n, args.batch_size)
-    del train_iter
+            # to anybody who wants to shorten this to a lambda: have you tried putting a breakpoint in a lambda?
+            def sample_fn(_) -> Tensor:
+                samp = next(iter_)
+                return samp[0]
+            features[source_name] = K.evaluation.compute_features(accelerator, sample_fn, extractor, args.evaluate_n, args.batch_size)
+        if accelerator.is_main_process:
+            fid = K.evaluation.fid(features['pred'], features['target'])
+            kid = K.evaluation.kid(features['pred'], features['target'])
+            print(f'FID: {fid.item():g}, KID: {kid.item():g}')
+        del iter_
 
 
 
